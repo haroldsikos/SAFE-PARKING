81379dae0ff2a956620a5b3e932b7011a083685a
DSR124 <246810diegosr@gmail.com>
2023-09-03 15:26:15 -0500

    agregar sus metodos y atributos a la clase vehiculo

--- src/main/java/pe/edu/upc/aaw/safe_parking/dtos/Vehiculo.java
+++ src/main/java/pe/edu/upc/aaw/safe_parking/dtos/Vehiculo.java
@@ -1,5 +1,70 @@
 package pe.edu.upc.aaw.safe_parking.dtos;
 
 public class Vehiculo {
+    private int idVehiculo;
+    private String PlacaVehiculo;
+    private String CAtegoriaVehiculo;
+    private String COlorVehiculo;
+    private String MArcaVehiculo;
+    private double TamanioVehiculo;
+    private String TarjetaPropiedadVehiculo;
 
+    public Vehiculo() {
+    }
+
+    public void setIdVehiculo(int idVehiculo) {
+        this.idVehiculo = idVehiculo;
+    }
+
+    public void setPlacaVehiculo(String placaVehiculo) {
+        PlacaVehiculo = placaVehiculo;
+    }
+
+    public void setCAtegoriaVehiculo(String CAtegoriaVehiculo) {
+        this.CAtegoriaVehiculo = CAtegoriaVehiculo;
+    }
+
+    public void setCOlorVehiculo(String COlorVehiculo) {
+        this.COlorVehiculo = COlorVehiculo;
+    }
+
+    public void setMArcaVehiculo(String MArcaVehiculo) {
+        this.MArcaVehiculo = MArcaVehiculo;
+    }
+
+    public void setTamanioVehiculo(double tamanioVehiculo) {
+        TamanioVehiculo = tamanioVehiculo;
+    }
+
+    public void setTarjetaPropiedadVehiculo(String tarjetaPropiedadVehiculo) {
+        TarjetaPropiedadVehiculo = tarjetaPropiedadVehiculo;
+    }
+
+    public int getIdVehiculo() {
+        return idVehiculo;
+    }
+
+    public String getPlacaVehiculo() {
+        return PlacaVehiculo;
+    }
+
+    public String getCAtegoriaVehiculo() {
+        return CAtegoriaVehiculo;
+    }
+
+    public String getCOlorVehiculo() {
+        return COlorVehiculo;
+    }
+
+    public String getMArcaVehiculo() {
+        return MArcaVehiculo;
+    }
+
+    public double getTamanioVehiculo() {
+        return TamanioVehiculo;
+    }
+
+    public String getTarjetaPropiedadVehiculo() {
+        return TarjetaPropiedadVehiculo;
+    }
 }
