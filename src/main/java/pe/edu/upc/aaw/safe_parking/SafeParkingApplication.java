ce5e27bedc2d994e8dba2653996369b4a8cb83b3
DSR124 <246810diegosr@gmail.com>
2023-09-03 15:11:12 -0500

    crear el proyecto

---
+++ src/main/java/pe/edu/upc/aaw/safe_parking/SafeParkingApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.aaw.safe_parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeParkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/aaw/safe_parking/SafeParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.aaw.safe_parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
